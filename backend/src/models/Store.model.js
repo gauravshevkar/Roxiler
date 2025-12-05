7f7efe8c6ff686e4dbc5d8b91b3190a8f2a44f4f
Gaurav Shevkar <gauravshevkar06@gmail.com>
2025-12-06 00:46:23 +0530

    Update Store.model.js

--- backend/src/models/Store.model.js
+++ backend/src/models/Store.model.js
@@ -8,11 +8,7 @@ const Store = sequelize.define(
 
     name: { type: DataTypes.STRING(100), allowNull: false },
 
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: { isEmail: true },
-    },
+
 
     address: { type: DataTypes.STRING(400), allowNull: false },
 
