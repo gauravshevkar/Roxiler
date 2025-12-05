4bf9ced6498d50469de5f8044ee7b21d5ce4704e
Gaurav Shevkar <gauravshevkar06@gmail.com>
2025-12-05 20:29:16 +0530

    Update API base URL in axios configuration

--- frontend/src/api/axiosConfig.jsx
+++ frontend/src/api/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://roxiler-frontend-7qd6.onrender.com/",
+  baseURL: "https://roxiler-backend1.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
